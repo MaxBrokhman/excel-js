d58129d8127772fa14749e0537635a7721c4a400
MaxBrohman <maksimbrochman@gmail.com>
2020-06-12 14:31:13 +0300

    observer added

---
+++ src/core/UpdateObserver.ts
@@ -0,0 +1,29 @@
+type TData = Record<string, string | number>
+type TCallback = (data?: TData) => void
+
+class UpdateObserver {
+  public subscriptions: Record<string, Array<TCallback>> = Object.create(null)
+
+  public subscribe(event: string, callback: () => void) {
+    if (this.subscriptions[event]) {
+      this.subscriptions[event].push(callback)
+    } else {
+      this.subscriptions[event] = [callback]
+    }
+  }
+
+  public unsubscribe(event: string, callback: () => void) {
+    if (this.subscriptions[event]) {
+      this.subscriptions[event] =
+        this.subscriptions[event].filter((sub) => sub !== callback)
+    }
+  }
+
+  public dispatch(event: string, data?: TData) {
+    if (this.subscriptions[event]) {
+      this.subscriptions[event].forEach((sub) => sub(data))
+    }
+  }
+}
+
+export const updater = new UpdateObserver()
